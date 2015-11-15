cbc2b387968497a3ddb3939acd173b2265b027e5
Oscar Tanner <oscarpanda@gmail.com>
2015-11-15 21:05:50 -0200

    Added fuction to find supplier
    Added function that finds the supplier by its name and address.

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,5 @@
 class Supplier < ActiveRecord::Base
+  def self.find_by_name_and_address(name, address)
+    Supplier.where(name: name, address: address).first
+  end
 end
--- test/models/supplier_test.rb
+++ test/models/supplier_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class SupplierTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_find_by_name_and_address
+    @supplier1 = suppliers(:supplier1)
+    assert_equal(@supplier1,
+                 Supplier.find_by_name_and_address(@supplier1.name,
+                                                   @supplier1.address))
+    @supplier2 = suppliers(:supplier2)
+    assert_equal(@supplier2,
+                 Supplier.find_by_name_and_address(@supplier2.name,
+                                                   @supplier2.address))
+    assert_not_equal(@supplier1,
+                     Supplier.find_by_name_and_address(@supplier2.name,
+                                                       @supplier2.address))
+    assert_nil(Supplier.find_by_name_and_address(@supplier1.name,
+                                                 @supplier2.address))
+  end
 end
