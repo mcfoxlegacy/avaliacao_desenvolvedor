15f86312d66637a5a1409205b7d1a06d3e907329
Oscar Tanner <oscarpanda@gmail.com>
2015-11-15 22:51:42 -0200

    Created buyer find function
    Create function that finds a buyer by name or creates a new one if no
    one exists

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,12 @@
 class Buyer < ActiveRecord::Base
+  # Overrides the find_by_name method to create the new buyer if
+  # there are no other with the name passed as parameter
+  def self.find(name)
+    buyer = find_by_name(name)
+    if buyer.nil?
+      Buyer.create(name: name)
+    else
+      buyer
+    end
+  end
 end
--- test/models/buyer_test.rb
+++ test/models/buyer_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class BuyerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_find_by_name
+    @buyer1 = buyers(:buyer1)
+    assert_equal(@buyer1, Buyer.find_by_name(@buyer1.name))
+
+    @buyer2 = buyers(:buyer2)
+    assert_equal(@buyer2, Buyer.find_by_name(@buyer2.name))
+  end
+
+  def test_find_by_name_new_name
+    new_name = 'Buyer 3'
+    buyer = Buyer.find(new_name)
+    assert_not_nil(buyer)
+    assert_not_nil(buyer.id)
+    assert_not_nil(buyer.created_at)
+    assert_not_nil(buyer.updated_at)
+    assert_equal(new_name, buyer.name)
+  end
 end
