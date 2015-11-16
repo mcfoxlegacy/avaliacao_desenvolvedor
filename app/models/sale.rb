04c96890032e9604d40615ec45208550e88371da
Oscar Tanner <oscarpanda@gmail.com>
2015-11-15 23:02:26 -0200

    Create function to import a sale's line

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,13 @@
 class Sale < ActiveRecord::Base
   belongs_to :buyer
   belongs_to :supplier
+
+  def import(line)
+    line = line.split("\t")
+    self.buyer = Buyer.find(line[0])
+    self.description = line[1]
+    self.price = line[2].to_f
+    self.quantity = line[3].to_i
+    self.supplier = Supplier.find_by_name_and_address(line[5], line[4])
+  end
 end
--- test/models/sale_test.rb
+++ test/models/sale_test.rb
@@ -1,7 +1,16 @@
+# coding: utf-8
 require 'test_helper'
 
 class SaleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_import
+    line = "João Silva	R$10 off R$20 of food	10.0	2	987 Fake St	Bob's Pizza"
+    sale = Sale.new
+    sale.import(line)
+    assert_equal(sale.buyer.name, 'João Silva')
+    assert_equal(sale.description, 'R$10 off R$20 of food')
+    assert_equal(sale.price, 10.0)
+    assert_equal(sale.quantity, 2)
+    assert_equal(sale.supplier.address, '987 Fake St')
+    assert_equal(sale.supplier.name, "Bob's Pizza")
+  end
 end
