5ca7b7d0c78e397bf77b73066320c341a050b4bf
Oscar Tanner <oscarpanda@gmail.com>
2015-11-15 23:08:59 -0200

    Fixed supplier_test metric problems

--- test/models/supplier_test.rb
+++ test/models/supplier_test.rb
@@ -7,19 +7,21 @@ class SupplierTest < ActiveSupport::TestCase
                  Supplier.find_by_name_and_address(@supplier1.name,
                                                    @supplier1.address))
     @supplier2 = suppliers(:supplier2)
-    assert_equal(@supplier2,
-                 Supplier.find_by_name_and_address(@supplier2.name,
-                                                   @supplier2.address))
+    assert_equal(@supplier2, Supplier.find_by_name_and_address(@supplier2.name,
+                                                               @supplier2.address))
     assert_not_equal(@supplier1,
                      Supplier.find_by_name_and_address(@supplier2.name,
                                                        @supplier2.address))
+  end
 
+  def test_find_by_name_and_address_new
+    @supplier1 = suppliers(:supplier1)
+    @supplier2 = suppliers(:supplier2)
     supplier = Supplier.find_by_name_and_address(@supplier1.name,
                                                  @supplier2.address)
     assert_not_nil(supplier)
     assert_not_nil(supplier.id)
     assert_not_nil(supplier.created_at)
-    assert_not_nil(supplier.updated_at)
     assert_equal(@supplier1.name, supplier.name)
     assert_equal(@supplier2.address, supplier.address)
   end
