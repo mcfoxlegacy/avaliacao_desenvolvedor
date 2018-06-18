171cf3828732ef26d009207c646f203195ee6355
Jean Brito <jeanfbrito@gmail.com>
2018-06-18 00:39:28 -0300

    ignore the first line of the file

--- app/app/controllers/file_imports_controller.rb
+++ app/app/controllers/file_imports_controller.rb
@@ -10,18 +10,23 @@ class FileImportsController < ApplicationController
     @imported_file = FileImport.new
     @imported_file.filename = file_import_params[:file].original_filename
     @imported_file.content = File.read(file_import_params[:file].tempfile)
+    first_line = true
     if @imported_file.save
       @imported_file.content.each_line do |record|
-        @imported_sale = Sale.new
-        data = record.chomp.split("\t")
-        @imported_sale.client = data[0]
-        @imported_sale.description = data[1]
-        @imported_sale.unit_price = data[2]
-        @imported_sale.quantity = data[3]
-        @imported_sale.address = data[4]
-        @imported_sale.supplier = data[5]
-        @imported_sale.file_import = @imported_file
-        @imported_sale.save
+        if first_line
+          first_line = false
+        else
+          @imported_sale = Sale.new
+          data = record.chomp.split("\t")
+          @imported_sale.client = data[0]
+          @imported_sale.description = data[1]
+          @imported_sale.unit_price = data[2]
+          @imported_sale.quantity = data[3]
+          @imported_sale.address = data[4]
+          @imported_sale.supplier = data[5]
+          @imported_sale.file_import = @imported_file
+          @imported_sale.save
+        end
       end
     redirect_to @imported_file
     else
