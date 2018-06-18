992515e76094a4637806d967683cd5f63c906fbc
Jean Brito <jeanfbrito@gmail.com>
2018-06-18 00:32:22 -0300

    importing file, saving and showing the result

--- app/app/controllers/file_imports_controller.rb
+++ app/app/controllers/file_imports_controller.rb
@@ -4,13 +4,26 @@ class FileImportsController < ApplicationController
   end
   def show
     @file_import = FileImport.find(params[:id])
+    @data_imported = @file_import.sales
   end
   def create
     @imported_file = FileImport.new
     @imported_file.filename = file_import_params[:file].original_filename
     @imported_file.content = File.read(file_import_params[:file].tempfile)
     if @imported_file.save
-      redirect_to @imported_file
+      @imported_file.content.each_line do |record|
+        @imported_sale = Sale.new
+        data = record.chomp.split("\t")
+        @imported_sale.client = data[0]
+        @imported_sale.description = data[1]
+        @imported_sale.unit_price = data[2]
+        @imported_sale.quantity = data[3]
+        @imported_sale.address = data[4]
+        @imported_sale.supplier = data[5]
+        @imported_sale.file_import = @imported_file
+        @imported_sale.save
+      end
+    redirect_to @imported_file
     else
       redirect_to file_imports_url
     end
