529f7a3bb6aec0d5373545a8f2cfe02ed39d8b46
Jean Brito <jeanfbrito@gmail.com>
2018-06-18 01:01:49 -0300

    add income calculation

--- app/app/models/file_import.rb
+++ app/app/models/file_import.rb
@@ -1,3 +1,10 @@
 class FileImport < ApplicationRecord
   has_many :sales
+  def income
+    income = 0
+    self.sales.each do |sale|
+      income = income + sale.unit_price * sale.quantity
+    end
+    return income
+  end
 end
